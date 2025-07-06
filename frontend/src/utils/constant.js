409a21b0f60c6ed49cef50afbec4a93903ca70e5
Sandeshj458 <sandeshjadhav45458@gmail.com>
2025-07-06 20:49:57 +0530

    Fix start script and Node version for production deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
 const deployLink = import.meta.env.VITE_RENDER_DEPLOY_LINK;
 
-export const USER_API_END_POINT = `${deployLink}/api/v1/user`;
-export const JOB_API_END_POINT = `${deployLink}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${deployLink}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${deployLink}/api/v1/company`;
+export const USER_API_END_POINT = `${deployLink}api/v1/user`;
+export const JOB_API_END_POINT = `${deployLink}api/v1/job`;
+export const APPLICATION_API_END_POINT = `${deployLink}api/v1/application`;
+export const COMPANY_API_END_POINT = `${deployLink}api/v1/company`;
